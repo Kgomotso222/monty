0a2104897ed015ea38e3b2747ceeb03e6c239c8e
Kgomotso222 <138695830+Kgomotso222@users.noreply.github.com>
2023-11-17 00:22:23 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 #include "monty.h"
-
 bus_t bus = {NULL, NULL, NULL, 0};
-
 /**
- * main - monty code
- * @argc: arguments
- * @argv: argumentv
- * Return: success geos to 0
- */
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
 	char *content;
@@ -43,5 +41,5 @@ int main(int argc, char *argv[])
 	}
 	free_stack(stack);
 	fclose(file);
-	return (0);
+return (0);
 }
